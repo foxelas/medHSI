0cf29fdc7dd280c895dff49bffe0240166aee4f1
foxelas <32893423+foxelas@users.noreply.github.com>
2021-12-22 11:38:13 +0900

    Update extinction plots

--- tools/matlab/utilities/PlotChromophoreAbsorptionSpectra.m
+++ tools/matlab/utilities/PlotChromophoreAbsorptionSpectra.m
@@ -6,16 +6,23 @@ function [extCoeffEumelanin2, extCoeffHbO, extCoeffHbR] = PlotChromophoreAbsorpt
 %   [extCoeffEumelanin2, extCoeffHbO, extCoeffHbR] = PlotChromophoreAbsorptionSpectra();
 
 importDir = config.GetSetting('importDir');
+pheomelaninFilename = 'pheomelanin_absroption.csv';
 eumelaninFilename = 'eumelanin_absroption.csv';
 hbFilename = 'hb_absorption_spectra_prahl.csv';
 eumelaninData = delimread(fullfile(importDir, eumelaninFilename), ',', 'num');
 eumelaninData = eumelaninData.num;
 hbData = delimread(fullfile(importDir, hbFilename), ',', 'num');
 hbData = hbData.num;
+pheomelaninData = delimread(fullfile(importDir, pheomelaninFilename), ',', 'num');
+pheomelaninData = pheomelaninData.num;
 
 eumelaninLambda = eumelaninData(:, 1);
 % extCoeffEumelanin1 = eumelaninData(:, 2);
 extCoeffEumelanin2 = eumelaninData(:, 3);
+
+pheomelaninLambda = pheomelaninData(:, 1);
+% extCoeffPheomelanin1 = pheomelaninData(:, 2);
+extCoeffPheomelanin2 = pheomelaninData(:, 3);
 
 % hbAmount = 150; %   A typical value of x for whole blood is x=150 g Hb/liter.
 % convertHbfun = @(x) 2.303 * hbAmount * x / 64500;
@@ -25,13 +32,17 @@ extCoeffHbR = hbData(:, 3);
 % absCoeffHbO = convertHbfun(extCoeffHbO);
 % absCoeffHbR = convertHbfun(extCoeffHbR);
 
+% (moles/liter) = M
+ 
 fig = figure(1);
 clf;
 hold on;
 %plot(eumelaninLambda, extCoeffEumelanin1, 'DisplayName', 'Eumelanin1', 'LineWidth', 2); %cm-1 / (mg/ml)
 plot(eumelaninLambda, extCoeffEumelanin2, 'DisplayName', 'Eumelanin', 'LineWidth', 2); %cm-1 / (moles/liter)
-plot(hbLambda, extCoeffHbO, 'DisplayName', 'HbO', 'LineWidth', 2); %cm-1/M
-plot(hbLambda, extCoeffHbR, 'DisplayName', 'HbR', 'LineWidth', 2); %cm-1/M
+plot(pheomelaninLambda, extCoeffPheomelanin2, 'DisplayName', 'Pheomelanin', 'LineWidth', 2); %cm-1 / (moles/liter)
+plot(hbLambda, extCoeffHbR, 'DisplayName', 'Hb', 'LineWidth', 2); %cm-1/M
+plot(hbLambda, extCoeffHbO, 'DisplayName', 'HbO_2', 'LineWidth', 2); %cm-1/M
+
 hold off
 % xlim([300, 800]);
 xlabel('Wavelength (nm)', 'FontSize', 15);
