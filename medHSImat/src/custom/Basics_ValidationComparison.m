0431c8c1e9cf8dc038126598273ecac3c1bd37b9
foxelas <32893423+foxelas@users.noreply.github.com>
2022-06-06 00:08:10 +0900

    update figure

--- medHSImat/src/custom/Basics_ValidationComparison.m
+++ medHSImat/src/custom/Basics_ValidationComparison.m
@@ -4,9 +4,16 @@ dirs = {'D:\elena\mspi\output\pslRaw32Augmented\python-test\validation\2022-06-0
     'D:\elena\mspi\output\pslRaw32Augmented\python-test\validation\2022-06-05_sm_resnet_pretrained_optimized\0_performance.mat'};
 
 names = {'Resnet', 'Pretrained Resnet', '3D CNN'};
+close all;
 figure(1);
 
-fprintf('Accuracy & Precision & Recall & JC & AUC \n');
+folds = 13;
+hasSens = true;
+if hasSens 
+    fprintf('Accuracy & Sensitivity & Specificity & JC & AUC \n');
+else
+    fprintf('Accuracy & Precision & Recall & JC & AUC \n');
+end
 
 hold on
 for i = 1:numel(dirs)
@@ -14,17 +21,25 @@ for i = 1:numel(dirs)
     
     v = cell2mat(testEval);
 
-    iouVals1 = [v.val_iou_score];
-    xVals1 = 1:13;
+    iouVals1 = [v.val_iou_score] * 100;
+    xVals1 = 1:folds;
     plot(xVals1, iouVals1, 'DisplayName', names{i}, 'LineWidth', 2);
     
     auc = mean(auc_val_);
     
-    folds = 13;
-    fprintf('%s & %.2f (%.2f) & %.2f (%.2f) & %.2f (%.2f) & %.2f (%.2f) & %.3f\n', ...
-        names{i}, mean([v.val_accuracy]) * 100, std([v.val_accuracy]) * 100 / 13, mean([v.val_precision]) * 100, std([v.val_precision]) * 100 / 13, ...
-        mean([v.val_recall]) * 100, std([v.val_recall]) * 100 / 13, mean([v.val_iou_score]) * 100, std([v.val_iou_score]) * 100 / 13, auc);
-
+    sensitivities = [v.val_true_positives] ./ ([v.val_true_positives] + [v.val_false_negatives]);
+    specificities = [v.val_true_negatives] ./ ([v.val_true_negatives] + [v.val_false_positives]);
+    
+    if ~hasSens
+        fprintf('%s & %.2f (%.2f) & %.2f (%.2f) & %.2f (%.2f) & %.2f (%.2f) & %.3f\n', ...
+            names{i}, mean([v.val_accuracy]) * 100, std([v.val_accuracy]) * 100 / folds, mean(sensitivities) * 100, std(sensitivities) * 100 / folds, ...
+            mean(specificities) * 100, std(specificities) * 100 / folds, mean([v.val_iou_score]) * 100, std([v.val_iou_score]) * 100 / folds, auc);
+    else
+        fprintf('%s & %.2f (%.2f) & %.2f (%.2f) & %.2f (%.2f) & %.2f (%.2f) & %.3f\n', ...
+            names{i}, mean([v.val_accuracy]) * 100, std([v.val_accuracy]) * 100 / folds, mean([v.val_precision]) * 100, std([v.val_precision]) * 100 / folds, ...
+            mean([v.val_recall]) * 100, std([v.val_recall]) * 100 / folds, mean([v.val_iou_score]) * 100, std([v.val_iou_score]) * 100 / folds, auc);
+    end
+    
     result = struct('Accuracy', mean([v.val_accuracy]) * 100, 'Precision', mean([v.val_precision]) * 100, ...
         'Recall',  mean([v.val_recall]) * 100, 'JC', mean([v.val_iou_score]) * 100, ...
         'AccSEM', std([v.val_accuracy]), 'PreSEM', std([v.val_precision]) * 100 / 13,'RecSEM',  std([v.val_recall]) * 100 / 13,'JCSEM', std([v.val_iou_score]) * 100 / 13);
@@ -34,5 +49,10 @@ hold off
 
 legend('FontSize', 15);
 xlabel('Patient Test Fold', 'FontSize', 15);
-ylabel('IOU', 'FontSize', 15);
+ylabel('Jaccard Coefficient', 'FontSize', 15);
+xlim([1,13]);
+xticks(1:13);
+ylim([0,100]);
+yticks(0:10:100);
+
 
